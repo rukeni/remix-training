aa92de883bc33a553f6f345efcda33787e581cf7
DESKTOP-4KHFB1C\rukeni <rukeni@naver.com>
2022-07-14 23:27:50 +0900

    연습용 리믹스 블로그 셋팅

---
+++ blog/.eslintrc.js
@@ -0,0 +1,18 @@
+/**
+ * @type {import('@types/eslint').Linter.BaseConfig}
+ */
+module.exports = {
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "@remix-run/eslint-config/jest-testing-library",
+    "prettier",
+  ],
+  settings: {
+    jest: {
+      version: 27,
+    },
+  },
+  plugins: ["prettier"],
+  parserOptions: { project: "tsconfig.json" },
+};
